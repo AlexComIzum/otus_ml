f534831fd0d1f6eae21ef9181ef759687fca044e
AlexComIzum <alexmagsh@yandex.ru>
2024-06-23 22:25:45 +0300

    homework hw_14

---
+++ ml_basic/exercises/14_hw/hw_2/exceptions.py
@@ -0,0 +1,37 @@
+"""
+Объявите следующие исключения:
+- LowFuelError
+- NotEnoughFuel
+- CargoOverload
+"""
+
+class LowFuelError(Exception):
+    # Constructor or Initializer
+    def __init__(self, value):
+        self.value = value
+
+    # __str__ is to print() the value
+    def __str__(self):
+        return self.value
+
+
+class NotEnoughFuel(Exception):
+
+    # Constructor or Initializer
+    def __init__(self, value):
+        self.value = value
+
+    # __str__ is to print() the value
+    def __str__(self):
+        return self.value
+
+
+class CargoOverload(Exception):
+
+    # Constructor or Initializer
+    def __init__(self, value):
+        self.value = value
+
+    # __str__ is to print() the value
+    def __str__(self):
+        return self.value
